505ec76dcb36e584151658a25da23bcd67799082
Alvin <alvinl70@nycstudents.net>
2023-03-02 01:37:40 -0500

    u

--- src/MovieCollectionRunner.java
+++ src/MovieCollectionRunner.java
@@ -1,2 +1,10 @@
-public class MovieCollectionRunner {
+import java.util.ArrayList;
+
+public class MovieCollectionRunner
+{
+    public static void main(String arg[])
+    {
+        MovieCollection myCollection = new MovieCollection("src/movies_data.csv");
+        myCollection.menu();
+    }
 }
